46119449614818e7835e56ae6b8fb10f6ec7527a
TokiLava <tokilava@gmail.com>
2023-09-29 10:47:11 +0200

    Pusher proximity measure code

--- app/src/main/java/com/example/mobileandwearabletechnologyprojekt/MainActivity.java
+++ app/src/main/java/com/example/mobileandwearabletechnologyprojekt/MainActivity.java
@@ -1,76 +1,63 @@
 package com.example.mobileandwearabletechnologyprojekt;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.mobileandwearabletechnologyprojekt.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private SensorManager sensorManager;
+    private Sensor proximitySensor;
+    private TextView proximityStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        proximityStatus = findViewById(R.id.proximityStatus);
 
-        setSupportActionBar(binding.toolbar);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (proximitySensor == null) {
+            proximityStatus.setText("Proximity sensor not available on this device");
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float proximityValue = sensorEvent.values[0];
+
+        if (proximityValue < proximitySensor.getMaximumRange()) {
+            // Object is near the proximity sensor
+            proximityStatus.setText("Near");
+        } else {
+            // Object is far from the proximity sensor
+            proximityStatus.setText("Far");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not used in this example
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
