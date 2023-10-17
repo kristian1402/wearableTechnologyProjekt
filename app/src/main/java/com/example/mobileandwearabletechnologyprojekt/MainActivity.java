5f67000220479baef3e893f0a3daf2042b083d5c
kristian1402 <ksaren20@student.aau.dk>
2023-10-17 12:22:04 +0200

    open camera

--- app/src/main/java/com/example/mobileandwearabletechnologyprojekt/MainActivity.java
+++ app/src/main/java/com/example/mobileandwearabletechnologyprojekt/MainActivity.java
@@ -1,28 +1,48 @@
 package com.example.mobileandwearabletechnologyprojekt;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.SessionConfiguration;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.Surface;
+import android.view.TextureView;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor proximitySensor;
     private TextView proximityStatus;
-    private TextView textView2;
+    private CameraManager cameraManager;
+    private CameraDevice cameraDevice;
+    private CameraCaptureSession cameraCaptureSession;
+    private TextureView textureView;
 
     // Timer variables
     private boolean nearState = false;
     private long nearStartTime = 0;
     private Handler handler = new Handler(Looper.getMainLooper());
+    MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +50,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         proximityStatus = findViewById(R.id.proximityStatus);
-        textView2 = findViewById(R.id.textView2);
+        textureView = findViewById(R.id.textureView);
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
@@ -54,9 +75,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     public void run() {
                         long currentTime = System.currentTimeMillis();
                         if (currentTime - nearStartTime >= 2000) {
-                            // Object has been near for 2 seconds, change the color of textView2 to green
-                            textView2.setTextColor(getResources().getColor(android.R.color.black));
-                            textView2.setText("2 Seconds");
+                            openCameraForGestureRecognition();
                         }
                     }
                 }, 2000); // 2000 milliseconds = 2 seconds
@@ -70,9 +89,73 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             handler.removeCallbacksAndMessages(null);
         }
     }
+
+    private void openCameraForGestureRecognition() {
+        try {
+            String cameraId = cameraManager.getCameraIdList()[1]; // You may need to choose a specific camera here
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            cameraManager.openCamera(cameraId, new CameraDevice.StateCallback() {
+                @Override
+                public void onOpened(@NonNull CameraDevice camera) {
+                    cameraDevice = camera;
+
+                    try {
+                        Surface surface = new Surface(textureView.getSurfaceTexture());
+                        CaptureRequest.Builder captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                        captureRequestBuilder.addTarget(surface);
+
+                        cameraDevice.createCaptureSession(Collections.singletonList(surface), new CameraCaptureSession.StateCallback() {
+                            @Override
+                            public void onConfigured(@NonNull CameraCaptureSession session) {
+                                cameraCaptureSession = session;
+
+                                try {
+                                    cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(), null, null);
+                                } catch (CameraAccessException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            @Override
+                            public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                                // Handle configuration failure
+                            }
+                        }, null);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onDisconnected(@NonNull CameraDevice camera) {
+                    cameraDevice.close();
+                    cameraDevice = null;
+                }
+
+                @Override
+                public void onError(@NonNull CameraDevice camera, int error) {
+                    // Handle camera device error
+                }
+            }, null);
+
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Not used in this example
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 
     @Override
@@ -85,5 +168,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onPause() {
         super.onPause();
         sensorManager.unregisterListener(this);
+        if (cameraDevice != null) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
     }
 }
