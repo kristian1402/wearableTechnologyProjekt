68bf0fad784bc1c94d92bea954fad5f024f50a2e
Charlotte1997AAU <80900771+Charlotte1997AAU@users.noreply.github.com>
2023-11-17 10:19:59 +0100

    maybe

--- app/src/main/java/com/example/mobileandwearabletechnologyprojekt/MainActivity.java
+++ app/src/main/java/com/example/mobileandwearabletechnologyprojekt/MainActivity.java
@@ -13,12 +13,9 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-<<<<<<< Updated upstream
 import android.media.AudioManager;
-=======
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
->>>>>>> Stashed changes
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
